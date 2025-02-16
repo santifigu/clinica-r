794c7900a0a0b9e2ab69264db476bedbd02133bb
Santiago Sosa Figueredo <171152262+santifigu@users.noreply.github.com>
2025-02-16 06:21:36 -0300

    Actualizar comentarios y detalles proyecto_clínica.py

--- proyecto_clínica.py
+++ proyecto_clínica.py
@@ -1,17 +1,17 @@
-#Importamos bibliotecas
+# Importamos bibliotecas
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 import sqlite3
 
-#Desarrollo de la interfaz gráfica
+# Desarrollo de la interfaz gráfica
 ventana = tk.Tk()
 ventana.title('Clínica Rosalía')
 ventana.geometry("930x355+225+150")#930 355
 ventana.resizable(width=False, height=False)
 ventana.configure(background="lightblue")
 
-#Gestion de pacientes
+# Gestion de pacientes
 id = tk.StringVar()
 nombre = tk.StringVar()
 apellido = tk.StringVar()
@@ -19,7 +19,7 @@ dni = tk.StringVar()
 edad = tk.StringVar()
 obra_social = tk.StringVar()
 
-#Conexión a BBDD
+# Conexión a BBDD
 def conexionBBDD():
     conn = sqlite3.connect('base_pacientes')
     cursor = conn.cursor()
@@ -37,9 +37,9 @@ def conexionBBDD():
         messagebox.showinfo(title='Conexión', message='Base de Datos creada exitosamente.')
         conn.commit()
     except:
-        messagebox.showinfo(title='Conexión', message='Conexión exitosa.') #si esta funcion es para crear una base de datos dejamos el título ahí, si no ponemos 'pass' y listo si ya está creada
+        messagebox.showinfo(title='Conexión', message='Conexión exitosa.') # Si esta funcion es para crear una base de datos dejamos el título ahí, si no ponemos 'pass' y listo si ya está creada.
 
-#Eliminar la base de datos
+# Eliminar la base de datos
 def eliminarBBDD():
     conn = sqlite3.connect('base_pacientes')
     cursor = conn.cursor()
@@ -58,7 +58,7 @@ def eliminarBBDD():
     limpiarcampos()
     mostrar()
 
-#Salir de la aplicación
+# Salir de la aplicación
 def salir():
     conn = sqlite3.connect('base_pacientes')
     valor = messagebox.askquestion(title='Salir', message='¿Deseas salir y guardar los cambios?')
@@ -68,7 +68,7 @@ def salir():
     else:
         pass
 
-#Limpiar campos
+# Limpiar campos
 def limpiarcampos():
     id.set('')
     nombre.set('')
@@ -77,17 +77,17 @@ def limpiarcampos():
     edad.set('')
     obra_social.set('')
 
-#Info de la app
+# Info de la app
 def info():
     acerca = messagebox.showinfo(title='Información de la aplicación', message= """
     Aplicación Clínica
     Versión 1.0 
     """)
 
-#Insertar valores
-def registrar(event):#creamos una funcion llamada registrar la cual va a contener la funcion crear, la misma que se utiliza para registrar a los usuarios
+# Insertar valores
+def registrar(event):# Creamos una funcion llamada registrar la cual va a contener la funcion crear, la misma que se utiliza para registrar a los usuarios.
     crear()
-ventana.bind('<Return>', registrar)#y acá hacemos que apretando el enter llame a la función registrar creando o registrando los valores dentro de la tabla
+ventana.bind('<Return>', registrar)# Y acá hacemos que apretando el enter llame a la función registrar creando o registrando los valores dentro de la tabla.
 
 def crear():
     conn = sqlite3.connect('base_pacientes')
@@ -183,10 +183,10 @@ def mostrarfiltro10_12():
     conn.close()
 
 ################################################### Tabla ###############################################################
-tree = ttk.Treeview(height=10, columns=('#0', '#1', '#2', '#3','#4'))#heigth=le damos altura, columns=cantidad de columnas con numeral
+tree = ttk.Treeview(height=10, columns=('#0', '#1', '#2', '#3','#4'))# En el primer parámetro de 'Treeview' (height) le damos altura, en el segundo (columns) le damos la cantidad de columnas con numeral.
 tree.place(x=0, y=130)
-tree.column('#0', width=100) #especificamos la columna y le damos una anchura de 100
-tree.heading('#0', text='ID', anchor=tk.CENTER)#especificando de nuevo la columna, le ponemos en el parametro 'text' el nombre y con 'anchor' le decimos que la palabra quede centrada
+tree.column('#0', width=100) # Especificamos la columna y le damos una anchura de 100
+tree.heading('#0', text='ID', anchor=tk.CENTER) # Especificando de nuevo la columna, le adquirimos en el parametro 'text' el nombre, y con 'anchor' le decimos que la palabra quede centrada.
 tree.heading('#1', text='Nombre', anchor=tk.CENTER)
 tree.heading('#2', text='Apellido', anchor=tk.CENTER)
 tree.column('#3', width=100)
@@ -195,7 +195,7 @@ tree.heading('#4', text='Obra Social', anchor=tk.CENTER)
 tree.column('#5', width=130)
 tree.heading('#5', text='DNI', anchor=tk.CENTER)
 
-#Actualizar- Sirve para cuando queremos actulizar los datos de alguien
+# Actualizar - Sirve para cuando queremos actualizar los datos de alguien
 def actualizar():
     conn = sqlite3.connect('base_pacientes')
     cursor = conn.cursor()
@@ -212,7 +212,7 @@ def actualizar():
     limpiarcampos()
     mostrar()
 
-#Clickear
+# Clickear
 def seleccionar(event):
     item = tree.identify('item', event.x, event.y)
     id.set(tree.item(item, 'text'))
@@ -224,8 +224,8 @@ def seleccionar(event):
 
 tree.bind('<Double-1>', seleccionar)
 
-#Borrar
-#borramos un paciente o empleado
+# Borrar
+# Borramos un paciente
 def borrar():
     conn = sqlite3.connect('base_pacientes')
     cursor = conn.cursor()
@@ -242,9 +242,9 @@ def borrar():
     limpiarcampos()
     mostrar()
 
-#Filtrar
-#filtramos pacientes
-#0-3
+# Filtrar
+# Filtramos pacientes
+# 0-3
 def filtrar0_3():
     conn = sqlite3.connect('base_pacientes')
     cursor = conn.cursor()
@@ -259,7 +259,7 @@ def filtrar0_3():
     limpiarcampos()
     mostrarfiltro0_3()
 
-#4-6
+# 4-6
 def filtrar4_6():
     conn = sqlite3.connect('base_pacientes')
     cursor = conn.cursor()
@@ -274,7 +274,7 @@ def filtrar4_6():
     limpiarcampos()
     mostrarfiltro4_6()
 
-#7-9
+# 7-9
 def filtrar7_9():
     conn = sqlite3.connect('base_pacientes')
     cursor = conn.cursor()
@@ -289,7 +289,7 @@ def filtrar7_9():
     limpiarcampos()
     mostrarfiltro7_9()
 
-#10-12
+# 10-12
 def filtrar10_12():
     conn = sqlite3.connect('base_pacientes')
     cursor = conn.cursor()
@@ -307,21 +307,21 @@ def filtrar10_12():
 
 ########################################################### Interfaz Gráfica #############################################################################    
 
-#Barra de menú
-#Inicio
-menubar = tk.Menu(ventana) #creamos una barra de menu u opciones en realidad, en la parte de arriba
-menubasedat = tk.Menu(menubar, tearoff=0) #creamos la estructura del menu para el inicio
-menubasedat.add_command(label='Crear/Conectar a la Base de Datos', command=conexionBBDD) #creamos o nos conectamos a la base de datos
-menubasedat.add_command(label='Eliminar Base de Datos', command=eliminarBBDD) #eliminar toda tabla, es decir, todo el registro
-menubasedat.add_command(label='Salir', command=salir) #salir de la app
-menubar.add_cascade(label='Inicio', menu=menubasedat) #vinculamos a la barra de menus
+# Barra de menú
+# Inicio
+menubar = tk.Menu(ventana) # Creamos una barra de menu u opciones en realidad, en la parte de arriba.
+menubasedat = tk.Menu(menubar, tearoff=0) # Creamos la estructura del menu para el inicio.
+menubasedat.add_command(label='Crear/Conectar a la Base de Datos', command=conexionBBDD) # Creamos o nos conectamos a la base de datos.
+menubasedat.add_command(label='Eliminar Base de Datos', command=eliminarBBDD) # Eliminar toda tabla, es decir, todo el registro.
+menubasedat.add_command(label='Salir', command=salir) # Salir de la app.
+menubar.add_cascade(label='Inicio', menu=menubasedat) # Vinculamos a la barra de menus.
 
-#Ayuda
-menuayuda = tk.Menu(menubar, tearoff=0) #creacion del menu ayuda 
-menuayuda.add_command(label='Info', command=info) #info de la app
-menubar.add_cascade(label='Info', menu=menuayuda)#enlazamos las opciones de arriba con el menu
+# Ayuda
+menuayuda = tk.Menu(menubar, tearoff=0) # Creacion del menu ayuda. 
+menuayuda.add_command(label='Info', command=info) # Info de la app.
+menubar.add_cascade(label='Info', menu=menuayuda) # Enlazamos las opciones de arriba con el menu.
 
-#Filtro - hacer su función
+# Filtro - hacer su función
 menufiltro = tk.Menu(menubar, tearoff=0)
 menufiltro.add_command(label='0-3', command=filtrar0_3)
 menufiltro.add_command(label='4-6', command=filtrar4_6)
@@ -329,36 +329,36 @@ menufiltro.add_command(label='7-9', command=filtrar7_9)
 menufiltro.add_command(label='10-12', command=filtrar10_12)
 menubar.add_cascade(label='Filtro', menu=menufiltro)
 
-ventana.config(menu=menubar) #agregamos las barras a la pantalla así aparecen
+ventana.config(menu=menubar) # Agregamos las barras a la pantalla así aparecen.
 
-#Caja
-e1 = tk.Entry(ventana, textvariable=id) #primero le indicamos donde o en que ventana va a estar y luego con qué la vamos a enlazar
+# Caja
+e1 = tk.Entry(ventana, textvariable=id) # Primero le indicamos donde o en que ventana va a estar y luego con qué la vamos a enlazar.
 
-#Entrada-Nombre
+# Entrada - Nombre
 l2 = tk.Label(ventana, text='Nombre', bg='lightblue')
 l2.place(x=50, y=10)   
 e2 = tk.Entry(ventana, textvariable=nombre, width=50)
 e2.place(x=130,y=10)
 
-#Entrada-Apellido
+# Entrada - Apellido
 l3 = tk.Label(ventana, text='Apellido', bg='lightblue')
 l3.place(x=50, y=40)   
 e3 = tk.Entry(ventana, textvariable=apellido, width=50)
 e3.place(x=130,y=40)
 
-#Entrada-Edad
+# Entrada - Edad
 l4 = tk.Label(ventana, text='Edad', bg='lightblue')
 l4.place(x=50, y=70)   
 e4 = tk.Entry(ventana, textvariable=edad, width=2)
 e4.place(x=130,y=70)
 
-#Entrada-Obra Social
+# Entrada - Obra Social
 l5 = tk.Label(ventana, text='Obra Social', bg='lightblue')
 l5.place(x=50, y=100)   
 e5 = tk.Entry(ventana, textvariable=obra_social)
 e5.place(x=130,y=100)
 
-#Entrada-Obra Social
+#Entrada - Obra Social
 l6 = tk.Label(ventana, text='DNI', bg='lightblue')
 l6.place(x=260, y=70)   
 e6 = tk.Entry(ventana, textvariable=dni)
@@ -367,15 +367,19 @@ e6.place(x=310,y=70)
 
 ########################### Botones #####################################
 
+# Registrar
 botregistrar = tk.Button(ventana, text='Registrar', command=crear)
 botregistrar.place(x=800, y=5)
 
+# Actualizar
 botactualizar = tk.Button(ventana, text='Actualizar', command=actualizar)
 botactualizar.place(x=800, y=35)
 
+# Ver lista de pacientes
 botmostrar = tk.Button(ventana, text='Ver Lista Pacientes', command=mostrar)
 botmostrar.place(x=780, y=65)
 
+# Eliminar
 boteliminar = tk.Button(ventana, text='Eliminar', bg='red', fg='white' ,command=borrar)
 boteliminar.place(x=800, y=95)
 
